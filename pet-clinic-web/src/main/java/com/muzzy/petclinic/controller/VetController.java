c1d2e80cc14d5b0dbe7cb3e620cbad9966cda724
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2019-12-05 14:22:46 +0100

    Add Vet index page controller Closes #7.

---
+++ pet-clinic-web/src/main/java/com/muzzy/petclinic/controller/VetController.java
@@ -0,0 +1,14 @@
+package com.muzzy.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/vet"})
+public class VetController {
+
+    @RequestMapping({"/","","index","index.html"})
+    public String getIndexPage(){
+        return "vet/index";
+    }
+}
