0bdbe88be49d763f942147610a53631370abcfa3
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2019-12-10 14:56:13 +0100

    Implement List Owners in Owners Controller. Closes #16

--- pet-clinic-web/src/main/java/com/muzzy/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/muzzy/petclinic/controller/OwnerController.java
@@ -1,14 +1,23 @@
 package com.muzzy.petclinic.controller;
+import com.muzzy.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequestMapping({"/owner"})
 public class OwnerController {
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
 
     @RequestMapping({"/","","index","index.html"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owner/index";
     }
 }
