24ac0e8ec3b527ee072a65239a000a36449525a8
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2019-12-12 15:24:30 +0100

    Go home

--- pet-clinic-web/src/main/java/com/muzzy/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/muzzy/petclinic/controller/OwnerController.java
@@ -1,5 +1,7 @@
 package com.muzzy.petclinic.controller;
+
 import com.muzzy.petclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OwnerController {
     private OwnerService ownerService;
 
+    @Autowired
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
 
-
-    @RequestMapping({"/","","index","index.html"})
-    public String getIndexPage(Model model){
-        model.addAttribute("owners",ownerService.findAll());
+    @RequestMapping({"/", "", "index", "index.html"})
+    public String getIndexPage(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 }
--- pet-clinic-web/src/main/java/com/muzzy/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/muzzy/petclinic/controller/VetController.java
@@ -1,14 +1,24 @@
 package com.muzzy.petclinic.controller;
 
-        import org.springframework.stereotype.Controller;
-        import org.springframework.web.bind.annotation.RequestMapping;
+import com.muzzy.petclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping({"/vet"})
 public class VetController {
+    private VetService vetService;
 
-    @RequestMapping({"/","","index","index.html"})
-    public String getIndexPage(){
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/", "", "index", "index.html"})
+    public String getIndexPage(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 }
