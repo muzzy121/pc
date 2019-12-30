bc2014a12cc5322ce54336dbbcc3d8b3af661d82
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2019-12-30 11:20:08 +0100

    Implemetation of SpecialtyServiceMap Closes #29

---
+++ pet-clinic-data/src/main/java/com/muzzy/petclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,34 @@
+package com.muzzy.petclinic.services.map;
+
+import com.muzzy.petclinic.model.Specialty;
+import com.muzzy.petclinic.services.SpecialtyService;
+
+import java.util.Set;
+
+public class SpecialtyServiceMap extends AbstractServiceMap<Specialty, Long> implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
