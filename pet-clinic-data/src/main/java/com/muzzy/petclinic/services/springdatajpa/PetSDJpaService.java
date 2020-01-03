8d5b47bc9f1e8a0ea6cde742e898720f817a3ea3
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2020-01-03 18:19:24 +0100

    Add new pet jpa repository Closes #43

---
+++ pet-clinic-data/src/main/java/com/muzzy/petclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,53 @@
+package com.muzzy.petclinic.services.springdatajpa;
+
+import com.muzzy.petclinic.model.Pet;
+import com.muzzy.petclinic.repository.PetRepository;
+import com.muzzy.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> petSet = new HashSet<>();
+        petRepository.findAll().forEach(petSet::add);
+        return petSet;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Pet findByName(String name) {
+        return petRepository.findFirstByName(name);
+    }
+}
+
