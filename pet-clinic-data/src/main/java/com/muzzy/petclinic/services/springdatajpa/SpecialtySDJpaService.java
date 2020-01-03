bc906d4c21df13c1bb726a75f60bc5fe747ad5ec
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2020-01-03 18:32:56 +0100

    Add new vet speciality jpa repository Closes #44

---
+++ pet-clinic-data/src/main/java/com/muzzy/petclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,47 @@
+package com.muzzy.petclinic.services.springdatajpa;
+
+import com.muzzy.petclinic.model.Specialty;
+import com.muzzy.petclinic.repository.SpecialtyRepository;
+import com.muzzy.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialtySet = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialtySet::add);
+        return specialtySet;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
