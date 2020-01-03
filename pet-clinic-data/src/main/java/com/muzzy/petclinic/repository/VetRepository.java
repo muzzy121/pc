3293848ea3471d05f1315b76822b5fd0b0923314
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2020-01-03 17:58:42 +0100

    Add new vet jpa repository Closes #41

--- pet-clinic-data/src/main/java/com/muzzy/petclinic/repository/VetRepository.java
+++ pet-clinic-data/src/main/java/com/muzzy/petclinic/repository/VetRepository.java
@@ -4,6 +4,10 @@ import com.muzzy.petclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.Set;
+
 @Repository
 public interface VetRepository extends CrudRepository<Vet,Long> {
+    Optional<Vet> findFirstByLastName(String lastName);
 }
