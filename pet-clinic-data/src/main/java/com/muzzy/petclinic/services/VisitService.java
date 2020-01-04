6c684ecf638ab66e72750c1cd328c34165f9d3ce
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2020-01-04 16:48:27 +0100

    Add new Visit Map Service Closes #46

---
+++ pet-clinic-data/src/main/java/com/muzzy/petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.muzzy.petclinic.services;
+
+import com.muzzy.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/muzzy/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,45 @@
+package com.muzzy.petclinic.services.map;
+
+import com.muzzy.petclinic.model.Visit;
+import com.muzzy.petclinic.services.PetService;
+import com.muzzy.petclinic.services.PetTypeService;
+import com.muzzy.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractServiceMap<Visit,Long> implements VisitService {
+//    private PetService petService;
+//    private PetTypeService petTypeService;
+//
+//    public VisitServiceMap(PetService petService, PetTypeService petTypeService) {
+//        this.petService = petService;
+//        this.petTypeService = petTypeService;
+//    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if(object.getPet() != null || object.getPet().getOwner() != null || object.getPet().getPetType() != null){
+            return super.save(object);
+        } else throw new RuntimeException("Visit have no proper pet object");
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
