d4c314781da7782c38ee509a541deacea546dac6
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2020-01-07 15:56:22 +0100

    Added surefire dependencies to pom

--- pet-clinic-web/src/test/java/com/muzzy/petclinic/PetclinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/muzzy/petclinic/PetclinicApplicationTests.java
@@ -1,4 +1,4 @@
-package petclinic;
+package com.muzzy.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PetclinicApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+    void contextLoads() {
 
+	}
 }
