24188ba87b2b5b9a2dc4ce3a74aadea4d9b0a556
muzzy121 <26625935+muzzy121@users.noreply.github.com>
2020-01-08 09:46:12 +0100

    Fix all issues with missing dependecies Fix PetclinicApplicationTest

---
+++ pet-clinic-web/src/test/java/com/muzzy/petclinic/PetclinicApplicationTest.java
@@ -0,0 +1,18 @@
+package com.muzzy.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PetclinicApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
